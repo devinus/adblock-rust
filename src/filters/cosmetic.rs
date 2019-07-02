2f02bd5f947ebc99f5a5c5f1daed33a1b19b6411
Anton Lazarev <antonok35@gmail.com>
2019-07-01 20:53:58 -0400

    add CosmeticFilter fields

--- src/filters/cosmetic.rs
+++ src/filters/cosmetic.rs
@@ -1,8 +1,38 @@
-pub struct CosmeticFilter(String);
+use serde::{Deserialize, Serialize};
+use crate::utils::Hash;
+
+bitflags! {
+    #[derive(Serialize, Deserialize)]
+    pub struct CosmeticFilterMask: u32 {
+        // Careful with checking for NONE - will always match
+        const NONE = 0;
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CosmeticFilter {
+    pub entities: Option<Vec<Hash>>,
+    pub hostnames: Option<Vec<Hash>>,
+    pub mask: CosmeticFilterMask,
+    pub not_entities: Option<Vec<Hash>>,
+    pub not_hostnames: Option<Vec<Hash>>,
+    pub raw_line: Option<String>,
+    pub selector: String,
+    pub style: Option<String>,
+}
 
 impl CosmeticFilter {
     pub fn parse(line: &str, _debug: bool) -> Result<CosmeticFilter, crate::filters::network::FilterError> {
         // TODO: unimplemented, just return rule as a string
-        Ok(CosmeticFilter(String::from(line)))
+        Ok(CosmeticFilter {
+            entities: None,
+            hostnames: None,
+            mask: CosmeticFilterMask::NONE,
+            not_entities: None,
+            not_hostnames: None,
+            raw_line: Some(String::from(line)),
+            selector: String::from(line),
+            style: None,
+        })
     }
 }
